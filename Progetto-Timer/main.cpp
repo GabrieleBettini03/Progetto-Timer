747bc17d54513734fde023262d28f251962e0c00
GabrieleBettini03 <gabriele.bettini0@gmail.com>
2026-02-06 19:32:59 +0100

    Adattamento contenuti al cambiare della pagina
    Aggiunto adattamento della posizione e dimensione dei contenuti della pagina in base al ridimensionamento della finestra, attraverso l'utilizzo di layout.
    
    TO DO: Aggiungere Unit Testing

--- Progetto-Timer/main.cpp
+++ Progetto-Timer/main.cpp
@@ -11,9 +11,14 @@
 #include <QMessageBox>
 #include <QDate>
 #include <QCalendarWidget>
-
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QStackedLayout>
 #include "AnalogClock.h"
 #include "Timer.h"
+
+int fxd_Btn_Wdt = 100;
+int fxd_Btn_Hgt = 50;
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -27,112 +32,187 @@ int main(int argc, char *argv[]) {
     //--------------------------------------------------- WIDGETS ----------------------------------------------------//
     //----------------------------------------------------------------------------------------------------------------//
 
-    //Label per orologio digitale
-    QLabel digitalTimeLabel(&window);
-    digitalTimeLabel.setAlignment(Qt::AlignCenter);
-    digitalTimeLabel.setGeometry(0, 100, 300,50);
-    digitalTimeLabel.setStyleSheet("font-size: 24px;");
+    QTimer timeTicker; //"Ticker" per scandire i secondi
 
-    //Widget per orologio analogico
-    AnalogClock analogClock(&window);
-    analogClock.setGeometry(50,10,200,200);
-    analogClock.hide();
-
-    QTimer time; //"Ticker" per scandire i secondi
     Timer timer; //Timer
 
-    //Button per cambiare il formato dell'ora
-    QPushButton buttonAMPM("AM/PM", &window);
-    buttonAMPM.setGeometry(75,250,150,50);
-    bool AMPM = false;
+    //===================//
+    //===== DISPLAY =====//
+    //===================//
 
-    //Button per cambiare Analogico/Digitale
-    QPushButton buttonAD("Analogico", &window);
-    buttonAD.setGeometry(75,320,150,50);
-    bool analogTime = false;
+    //Label per Orologio Digitale
+    QLabel digitalTimeLabel(&window);
+    digitalTimeLabel.setAlignment(Qt::AlignCenter);
+    digitalTimeLabel.setStyleSheet("font-size: 24px;");
 
-    //Button per passare a modalità Timer
-    QPushButton buttonT("Timer", &window);
-    buttonT.setGeometry(75,390,150,50);
-    bool timerScreen = false;
-
-    //SpinBox per il Timer
-    QSpinBox spinHours(&window);
-    QSpinBox spinMinutes(&window);
-    QSpinBox spinSeconds(&window);
-
-    QLabel labelSeparator_one(" :", &window);
-    QLabel labelSeparator_two(" :", &window);
-
-    //Ore
-    spinHours.setRange(0, 24);
-    spinHours.setValue(0);
-    spinHours.setSuffix(" H");
-    spinHours.setGeometry(8,120,82,40);
-
-    //Minuti
-    spinMinutes.setRange(0, 59);
-    spinMinutes.setValue(0);
-    spinMinutes.setSuffix(" m");
-    spinMinutes.setGeometry(110,120,82,40);
-
-    //Secondi
-    spinSeconds.setRange(0, 59);
-    spinSeconds.setValue(0);
-    spinSeconds.setSuffix(" s");
-    spinSeconds.setGeometry(212,120,82,40);
-
-    labelSeparator_one.setGeometry(95,125,10,30);
-    labelSeparator_two.setGeometry(197,125,10,30);
-
-    spinHours.hide();
-    spinMinutes.hide();
-    spinSeconds.hide();
-    labelSeparator_one.hide();
-    labelSeparator_two.hide();
+    //Widget e Layout per Orologio Analogico
+    AnalogClock analogClock(&window);
+    analogClock.setFixedSize(200,200);
+                                                                                        //Creo un altro layout e widget per centrare analogClock (a dimensione fissata), perchè non centrabile in QStackedLayout
+    QWidget *analogClockWidget = new QWidget;                                           //  QStackedLayout:
+    QHBoxLayout *analogClockLayout = new QHBoxLayout(analogClockWidget);                //   - Widget -> digitalTimeLabel
+                                                                                        //   - Widget -> QHBoxLayout:
+    analogClockLayout->addStretch();                                                    //                - analogClock
+    analogClockLayout->addWidget(&analogClock,0,Qt::AlignCenter);      //   - Widget -> ...
+    analogClockLayout->addStretch();
 
     //Label per il Timer
     QLabel timerLabel ("00 : 00 : 00",&window);
     timerLabel.setAlignment(Qt::AlignCenter);
     timerLabel.setStyleSheet("font-size: 24px;");
-    timerLabel.setGeometry(0,50,300,50);
-    timerLabel.hide();
 
-    //Button per Iniziare, Stoppare, e Resettare il Timer
-    QPushButton timerStart("Start", &window);
-    QPushButton timerPauseResume("Pause", &window);
-    QPushButton timerReset("Reset", &window);
-    timerStart.setGeometry(75,250,150,50);
-    timerPauseResume.setGeometry(75, 320, 70, 50);
-    timerReset.setGeometry(155, 320, 70, 50);
-    timerStart.hide();
-    timerPauseResume.hide();
-    timerReset.hide();
-
-    //Button per passare alla modalità "Date"
-    QPushButton buttonDate("Date", &window);
-    buttonDate.setGeometry(75,460,150,50);
-    bool dateScreen = false;
-
+    //Label per la Data e Widget per il Calendario
     QDate date = QDate::currentDate();
     QString dateString = date.toString("dd/MM/yyyy");
 
-    QLabel labelDate(&window);
-    labelDate.setAlignment(Qt::AlignCenter);
-    labelDate.setGeometry(0,50,300,50);
-    labelDate.setStyleSheet("font-size: 24px;");
-    labelDate.hide();
+    QLabel dateLabel(&window);
+    dateLabel.setAlignment(Qt::AlignCenter);
+    dateLabel.setStyleSheet("font-size: 24px;");
 
-    //Widget per il calendario
     QCalendarWidget calendar(&window);
-    calendar.setGeometry(10,120,280,300);
-    calendar.hide();
+    calendar.setMinimumSize(400,300);
+    calendar.setMaximumSize(1000,700);
+
+    QWidget *dateDisplayWidget = new QWidget;
+    QVBoxLayout *dateDisplayLayout = new QVBoxLayout(dateDisplayWidget);
+
+    dateDisplayLayout->addStretch();
+    dateDisplayLayout->addWidget(&dateLabel);
+    dateDisplayLayout->addSpacing(50);
+    dateDisplayLayout->addWidget(&calendar,0,Qt::AlignCenter);
+    dateDisplayLayout->addStretch();
+
+    QStackedLayout *displayLayout = new QStackedLayout;
+
+    displayLayout->addWidget(&digitalTimeLabel);
+    displayLayout->addWidget(analogClockWidget);
+    displayLayout->addWidget(&timerLabel);
+    displayLayout->addWidget(dateDisplayWidget);
+
+    //=============================================================//
+
+
+    //===================//
+    //=== TIMER INPUT ===//
+    //===================//
+
+    QSpinBox spinHours(&window);
+    QSpinBox spinMinutes(&window);
+    QSpinBox spinSeconds(&window);
+
+    spinHours.setRange(0, 24);
+    spinHours.setValue(0);
+    spinHours.setSuffix(" H");
+
+    spinMinutes.setRange(0, 59);
+    spinMinutes.setValue(0);
+    spinMinutes.setSuffix(" m");
+
+    spinSeconds.setRange(0, 59);
+    spinSeconds.setValue(0);
+    spinSeconds.setSuffix(" s");
+
+    QLabel sep1(" :", &window);
+    QLabel sep2(" :", &window);
+
+    QHBoxLayout *timeInputLayout = new QHBoxLayout;
+
+    timeInputLayout->addStretch();
+    timeInputLayout->addWidget(&spinHours);
+    timeInputLayout->addWidget(&sep1);
+    timeInputLayout->addWidget(&spinMinutes);
+    timeInputLayout->addWidget(&sep2);
+    timeInputLayout->addWidget(&spinSeconds);
+    timeInputLayout->addStretch();
+
+    QWidget timeInputWidget(&window);
+    timeInputWidget.setLayout(timeInputLayout);
+    timeInputWidget.setVisible(false);
+
+    //=============================================================//
+
+
+    //===================//
+    //== TIMER BUTTONS ==//
+    //===================//
+
+    QPushButton timerStart("Start", &window);
+    timerStart.setFixedSize(fxd_Btn_Wdt,fxd_Btn_Hgt);
+    QPushButton timerPauseResume("Pause", &window);
+    timerPauseResume.setFixedSize(fxd_Btn_Wdt,fxd_Btn_Hgt);
+    QPushButton timerReset("Reset", &window);
+    timerReset.setFixedSize(fxd_Btn_Wdt,fxd_Btn_Hgt);
+
+    QHBoxLayout *timerButtonsLayout = new QHBoxLayout;
+
+    timerButtonsLayout->addStretch();
+
+    timerButtonsLayout->addWidget(&timerStart);
+    timerButtonsLayout->addWidget(&timerPauseResume);
+    timerButtonsLayout->addWidget(&timerReset);
+
+    timerButtonsLayout->addStretch();
+
+    QWidget timerButtonsWidget(&window);
+    timerButtonsWidget.setLayout(timerButtonsLayout);
+    timerButtonsWidget.setVisible(false);
+
+    //=============================================================//
+
+    //==================//
+    //== MODE BUTTONS ==//
+    //==================//
+
+    QPushButton digitalTimeFormatButton("AM/PM", &window);
+    digitalTimeFormatButton.setFixedSize(fxd_Btn_Wdt,fxd_Btn_Hgt);
+    bool AMPM = false;
+    QPushButton digitalAnalogButton("Analogico", &window);
+    digitalAnalogButton.setFixedSize(fxd_Btn_Wdt,fxd_Btn_Hgt);
+    bool analogTime = false;
+    QPushButton timerScreenButton("Timer", &window);
+    timerScreenButton.setFixedSize(fxd_Btn_Wdt,fxd_Btn_Hgt);
+    bool timerScreen = false;
+    QPushButton dateButton("Date", &window);
+    dateButton.setFixedSize(fxd_Btn_Wdt,fxd_Btn_Hgt);
+    bool dateScreen = false;
+
+    QHBoxLayout *modeButtonsLayout = new QHBoxLayout;
+
+    modeButtonsLayout->addStretch();
+
+    modeButtonsLayout->addWidget(&digitalTimeFormatButton);
+    modeButtonsLayout->addWidget(&digitalAnalogButton);
+    modeButtonsLayout->addWidget(&timerScreenButton);
+    modeButtonsLayout->addWidget(&dateButton);
+
+    modeButtonsLayout->addStretch();
+
+    //=============================================================//
+
+    //===================//
+    //=== MAIN LAYOUT ===//
+    //===================//
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+
+    mainLayout->addLayout(displayLayout);
+    mainLayout->addWidget(&timeInputWidget);
+    mainLayout->addWidget(&timerButtonsWidget);
+    mainLayout->addLayout(modeButtonsLayout);
+    mainLayout->addStretch();
+
+    window.setLayout(mainLayout);
+
+    //=============================================================//
+
+
 
     //----------------------------------------------------------------------------------------------------------------//
     //------------------------------------------------- CONNESSIONI --------------------------------------------------//
     //----------------------------------------------------------------------------------------------------------------//
+
     //Aggiornamento tempo
-    QObject::connect(&time, &QTimer::timeout, [&]() {
+    QObject::connect(&timeTicker, &QTimer::timeout, [&]() {
         QString format = AMPM? "hh:mm:ss AP":"HH:mm:ss";
         QString time = QDateTime::currentDateTime().toString(format);
         digitalTimeLabel.setText(time);
@@ -171,49 +251,45 @@ int main(int argc, char *argv[]) {
     });
 
     //Segnale button AM/PM
-    QObject::connect(&buttonAMPM, &QPushButton::clicked, [&]() {
+    QObject::connect(&digitalTimeFormatButton, &QPushButton::clicked, [&]() {
         AMPM = !AMPM;
 
         QString buttonTextAMPM = AMPM ? "24h" : "AM/PM";
-        buttonAMPM.setText(buttonTextAMPM);
+        digitalTimeFormatButton.setText(buttonTextAMPM);
     });
 
     //Segnale button Digitale/Analogico
-    QObject::connect(&buttonAD, &QPushButton::clicked, [&]() {
+    QObject::connect(&digitalAnalogButton, &QPushButton::clicked, [&]() {
         analogTime = !analogTime;
 
-        digitalTimeLabel.setVisible(!analogTime * !timerScreen);
-        analogClock.setVisible(analogTime * !timerScreen);
+        if (!timerScreen && !dateScreen) {
+            if (analogTime) displayLayout->setCurrentIndex(1);
+            else  displayLayout->setCurrentIndex(0);
+        }
 
         QString buttonTextAD = analogTime ? "Digitale" : "Analogico";
-        buttonAD.setText(buttonTextAD);
+        digitalAnalogButton.setText(buttonTextAD);
     });
 
     //Segnale button Timer
-    QObject::connect(&buttonT, &QPushButton::clicked, [&]() {
+    QObject::connect(&timerScreenButton, &QPushButton::clicked, [&]() {
         timerScreen = !timerScreen;
+        if (dateScreen) {
+            dateScreen = false;
+            dateButton.setText("Data");
+        }
 
-        buttonAMPM.setVisible(!timerScreen);
-        buttonAD.setVisible(!timerScreen);
-        buttonDate.setVisible(!timerScreen);
+        if (timerScreen) displayLayout->setCurrentIndex(2);
+        else {
+            if (analogTime) displayLayout->setCurrentIndex(1);
+            else  displayLayout->setCurrentIndex(0);
+        }
 
-        digitalTimeLabel.setVisible(!analogTime * !timerScreen);
-        analogClock.setVisible(analogTime * !timerScreen);
-
-        timerStart.setVisible(timerScreen);
-        timerPauseResume.setVisible(timerScreen);
-        timerReset.setVisible(timerScreen);
-
-        spinHours.setVisible(timerScreen);
-        spinMinutes.setVisible(timerScreen);
-        spinSeconds.setVisible(timerScreen);
-        labelSeparator_one.setVisible(timerScreen);
-        labelSeparator_two.setVisible(timerScreen);
-
-        timerLabel.setVisible(timerScreen);
+        timeInputWidget.setVisible(timerScreen);
+        timerButtonsWidget.setVisible(timerScreen);
 
         QString buttonTextTimer = timerScreen ? "Orologio" : "Timer";
-        buttonT.setText(buttonTextTimer);
+        timerScreenButton.setText(buttonTextTimer);
     });
 
     //Segnale Button Timer Start
@@ -254,25 +330,28 @@ int main(int argc, char *argv[]) {
     });
 
     //Segnale Button Date
-    QObject::connect(&buttonDate, &QPushButton::clicked, [&]() {
+    QObject::connect(&dateButton, &QPushButton::clicked, [&]() {
         dateScreen = !dateScreen;
+        if (timerScreen) {
+            timerScreen = false;
+            timeInputWidget.setVisible(false);
+            timerButtonsWidget.setVisible(false);
+            timerScreenButton.setText("Timer");
+        }
 
-        buttonAMPM.setVisible(!dateScreen);
-        buttonAD.setVisible(!dateScreen);
+        if (dateScreen) displayLayout->setCurrentIndex(3);
+        else {
+            if (analogTime) displayLayout->setCurrentIndex(1);
+            else  displayLayout->setCurrentIndex(0);
+        }
 
-        digitalTimeLabel.setVisible(!analogTime * !dateScreen);
-        analogClock.setVisible(analogTime * !dateScreen);
-        buttonT.setVisible(!dateScreen);
-
-        labelDate.setVisible(dateScreen);
-        calendar.setVisible(dateScreen);
         QString buttonTextDate = dateScreen ? "Orario" : "Data";
-        buttonDate.setText(buttonTextDate);
+        dateButton.setText(buttonTextDate);
 
-        labelDate.setText(dateString);
+        dateLabel.setText(dateString);
     });
 
-    time.start(1000);
+    timeTicker.start(1000);
 
     window.show();
     return app.exec();
